5e4c5160596e0565a12c0c8014bf69791e695859
baturayucer <baturayucer@gmail.com>
2020-08-29 16:17:00 +0300

    Minor - change naming convention

--- src/main/java/com/baturayucer/hotelsearch/HotelSearchApiApplication.java
+++ src/main/java/com/baturayucer/hotelsearch/HotelSearchApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HotelsearchApiApplication {
+public class HotelSearchApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HotelsearchApiApplication.class, args);
+        SpringApplication.run(HotelSearchApiApplication.class, args);
     }
 
 }
