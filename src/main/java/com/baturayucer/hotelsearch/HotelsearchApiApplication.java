5fe1630300dd7f8174f2382f6255ca453b92f91c
baturayucer <baturayucer@gmail.com>
2020-08-29 16:15:37 +0300

    Minor - initial commit

---
+++ src/main/java/com/baturayucer/hotelsearch/HotelsearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.baturayucer.hotelsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelsearchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baturayucer/hotelsearch/HotelsearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baturayucer.hotelsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsearchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
