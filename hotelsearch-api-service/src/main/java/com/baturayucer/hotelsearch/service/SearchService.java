8ffd406a7946497d0c31a6a015aa607eb79355f7
baturayucer <baturayucer@gmail.com>
2020-08-30 12:28:44 +0300

    Minor(fix) - bean load problem solved

--- hotelsearch-api-service/src/main/java/com/baturayucer/hotelsearch/service/SearchService.java
+++
@@ -1,9 +0,0 @@
-package com.baturayucer.hotelsearch.service;
-
-import com.baturayucer.hotelsearch.service.model.SearchItemDto;
-import com.baturayucer.hotelsearch.service.model.SearchOutputDto;
-
-public interface SearchService {
-
-    SearchOutputDto searchDeals(SearchItemDto searchItemDto);
-}
